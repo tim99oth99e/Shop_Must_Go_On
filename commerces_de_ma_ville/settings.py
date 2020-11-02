b2ed42056adb9a90c9354c4764f078e5f404f7f7
tim99oth99e <58693721+tim99oth99e@users.noreply.github.com>
2020-11-02 11:17:32 +0100

    updated STATIC_ROOT

--- commerces_de_ma_ville/settings.py
+++ commerces_de_ma_ville/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where our uploaded files will be located in file system
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # where our uploaded files will be located in file system
